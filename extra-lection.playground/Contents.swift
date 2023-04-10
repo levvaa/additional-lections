9379f35ec299063ec42d0a3de6f990a559fe774c
Alex Levitski <levitski.work@gmail.com>
2023-04-11 02:10:08 +0800

    extra homework without tests

--- extra-lection.playground/Contents.swift
+++ extra-lection.playground/Contents.swift
@@ -1,54 +1,88 @@
+/// Сlass describes a truck that can pick up and drop off loads
+/// Method `pickUp`, that increase current capacity of truck, and `dropOff` that reduce carrent capacity.
+/// Capacity cannot be negative value.
 class Truck {
-    let weightCapacity: Int
+    /// determines the maximum volume of the truck
+    let weightCapacity: UInt
 
     // MARK: internal interface
 
-    init(maxWeightCapacity: Int) {
+    init(maxWeightCapacity: UInt) {
         weightCapacity = maxWeightCapacity
     }
-    /// This function increases the current weight. Сurrent weight cannot be more than 50000
-    /// - Parameters:the weight by which the current load will be increased
-    func pickUp(weight: Int) {
-        guard currentWeight + weight <= weightCapacity else { return }
+    /// This function increases the current weight of truck.
+    /// - Parameter weight: instance of class Load with current weight.
+    /// If the maximum capacity of the truck is exceeded, returns false.
+    func pickUp(weight: Load) {
 
-        currentWeight += weight
+        guard currentWeight + weight.loadWeight <= weightCapacity else { return }
+
+        currentWeight += weight.loadWeight
     }
 
     /// Function reduces the current weight. If the result is less than zero, returns false.
-    /// - Parameters: weight, that will be reduced
-    func dropOff(weight: Int) {
-        guard currentWeight - weight >= 0 else { return }
+    /// - Parameter weight: the value by which the current capacity will be reduced
+    /// If after unloading the scales of the total capacity of the truck, returns false
+    func dropOff(weight: Load) {
 
-        currentWeight = currentWeight - weight
+        currentWeight -= weight.loadWeight
     }
         // MARK: Private interface
-    private(set) var currentWeight = 0
+    private(set) var currentWeight: UInt = 0
 }
 
+///  The class describes the place where loads can be unloaded, and from where they can be picked up.
+///  Method `dropOffToStorage` describe  the process of unloading load into the warehouse. When load has unloaded,
+///  current capcity of storage encreasing. Method `putInTruck` describes the process of loading cargo onto a truck.
+///  At this moment, the current load of the warehouse decreases, and the truck increases.
+///  The store cannot store negative values.
 class Storage {
-    let storageCapacity: Int
 
-    init(maxStorageCapacity: Int) {
+    // MARK: Internal interface
+
+    /// determines the maximum capacity of the storage
+    let storageCapacity: UInt
+
+    init(maxStorageCapacity: UInt) {
         storageCapacity = maxStorageCapacity
-    ///
-    func dropOffToStorage(_ dropWeight: Int, _ truck: Truck) {
-        guard currentStorageWeight + dropWeight <= storageCapacity else { return }
-
-        truck.dropOff(weight: dropWeight)
-        currentStorageWeight += dropWeight
     }
+    /// This method describes the process of unloading a truck of a certain cargo in storage
+    /// When unloading a truck, its current load is reduced by the weight of the load
+    /// - Parameter dropLoad: instance of class Load
+    /// - Parameter truck: instance of class Truck
+    /// If the maximum capacity of the storage is exceeded, returns false.
+    func dropOffToStorage(dropLoad: Load, truck: Truck) {
+        guard currentStorageWeight + dropLoad.loadWeight <= storageCapacity else { return }
 
-    func putInTruck(putInWeight: Int, truck: Truck) {
-        guard currentStorageWeight - putInWeight >= 0 else { return }
+        truck.dropOff(weight: Load(loadWeight: dropLoad.loadWeight))
+        currentStorageWeight += dropLoad.loadWeight
+    }
+        /// The function reduces the current capacity of the storage and increases the current capacity of the truck
+        /// - Parameter putInWeight:instance of the class Load
+        /// - Parameter truck: instance of class  Truck.
+        /// If the maximum capacity of the truck is exceeded, returns false.
+    func putInTruck(putInWeight: Load, truck: Truck) {
+        guard truck.currentWeight + putInWeight.loadWeight <= truck.weightCapacity else { return }
 
-        truck.pickUp(weight: putInWeight)
-        currentStorageWeight -= putInWeight
+        truck.pickUp(weight: Load(loadWeight: putInWeight.loadWeight))
+        currentStorageWeight -= putInWeight.loadWeight
 
         }
-    }
 
-    private(set) var currentStorageWeight = 0
+    // MARK: Private interface
+
+    private(set) var currentStorageWeight: UInt = 0
 }
 
-    // TODO: class Load (описывает груз)
+/// Class describes a loads.
+/// Have one propety `loadWeight` that describes weight of load instance.
+class Load {
+
+    let loadWeight: UInt
+
+    init(loadWeight: UInt) {
+           self.loadWeight = loadWeight
+       }
+
+}
 
